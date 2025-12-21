79660efb7cd38d7f7261c31735cf903b64c35cd3
anekobtw <krolexonder@gmail.com>
2025-12-21 15:46:56 +0400

    set up the project

--- just/src/App.tsx
+++ just/src/App.tsx
@@ -1,7 +1,8 @@
 function App() {
   return (
-    <>
-    </>
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-6xl font-thin text-primary">Just MONITOR.</h1>
+    </div>
   )
 }
 
